904a872b3dec33bc809a6ecd1294ab0be7f98c4d
Michał Malinowski <malinowskimichal997@tlen.pl>
2021-09-27 17:34:41 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+uppercaseLetter1 = chr(random.randint(65, 90))
+uppercaseLetter2 = chr(random.randint(65, 90))
+lowercaseLetter1 = chr(random.randint(97, 122))
+lowercaseLetter2 = chr(random.randint(97, 122))
+digit1 = chr(random.randint(48, 57))
+digit2 = chr(random.randint(48, 57))
+punctuationSign1 = chr(random.randint(33, 47))
+punctuationSign2 = chr(random.randint(33, 47))
+
+def shuffle(string):
+    tempList = list(string)
+    random.shuffle(tempList)
+    return ''.join(tempList)
+
+password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + punctuationSign2
+password = shuffle(password)
+
+print(password)
